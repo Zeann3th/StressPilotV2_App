355e68f4810051272f994834467374060fd3977a
zeann3th <longhienly112@gmail.com>
2026-03-21 14:54:01 +0700

    integration

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,8 +1,7 @@
 class AppConfig {
   static const String appName = "Stress Pilot";
-  static const String apiBaseUrl = "http://localhost:52000";
+  static const String apiBaseUrl = "http://127.0.0.1:52000";
   static const String pluginBaseUrl = "http://localhost/plugin-service";
   static const String pluginCmsBaseUrl = "http://localhost/plugin-service-cms";
-  static const String nexusBaseUrl = "http://localhost:8081";
   static const String version = "0.1.0";
 }
