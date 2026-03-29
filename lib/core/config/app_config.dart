8dbbdd0cd49e8d412d57b91dd597f0e826cbd591
zeann3th <longhienly112@gmail.com>
2026-03-29 12:40:49 +0700

    fix: update plugin marketplace

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,8 +1,7 @@
 class AppConfig {
   static const String appName = "Stress Pilot";
   static const String apiBaseUrl = "http://127.0.0.1:52000";
-  static const String pluginBaseUrl = "http://localhost/plugin-service";
-  static const String pluginCmsBaseUrl = "http://localhost/plugin-service-cms";
+  static const String pluginCmsBaseUrl = "https://plugins-cms.stresspilot.zeann3th.com";
   static const String version = "0.1.0";
   static const String githubIssuesUrl = "https://github.com/Zeann3th/StressPilotV2_App/issues/new";
 }
